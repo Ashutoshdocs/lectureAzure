df7f9bd13db6083d765cbb006c93287f8a83c0e7
Ashutoshdocs <aksinghdocument@gmail.com>
2026-07-31 20:36:49 +0530

    Update stress.ps1

--- VMSS/stress.ps1
+++ VMSS/stress.ps1
@@ -1,15 +1,23 @@
+$script = @'
+sudo apt-get update
+sudo apt-get install -y stress-ng
+stress-ng --cpu 2 --timeout 600s
+'@
+
 $ids = az vmss list-instances `
-  -g vmss-demo-rg `
-  -n vmssdemo-vmss `
-  --query "[].instanceId" -o tsv
+-g vmss-demo-rg `
+-n vmssdemo-vmss `
+--query "[].instanceId" -o tsv
 
 foreach ($id in $ids) {
-    Write-Host "Starting CPU stress on instance $id..."
+
+    Write-Host "Starting stress on VM $id"
 
     az vmss run-command invoke `
       -g vmss-demo-rg `
       -n vmssdemo-vmss `
       --instance-id $id `
       --command-id RunShellScript `
-      --scripts "for i in 1 2; do (while :; do :; done) & done; sleep 600; kill \$(jobs -p)"
+      --scripts $script
+
 }
