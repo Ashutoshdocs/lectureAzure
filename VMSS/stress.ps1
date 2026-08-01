58566439821dab30fcd7a0885495c5aab5204a70
Ashutoshdocs <aksinghdocument@gmail.com>
2026-08-01 10:25:59 +0530

    Update stress.ps1

--- VMSS/stress.ps1
+++ VMSS/stress.ps1
@@ -1,23 +1,10 @@
-$script = @'
-sudo apt-get update
-sudo apt-get install -y stress-ng
-stress-ng --cpu 2 --timeout 600s
-'@
-
-$ids = az vmss list-instances `
--g vmss-demo-rg `
--n vmssdemo-vmss `
---query "[].instanceId" -o tsv
+$ids = az vmss list-instances -g vmss-demo-rg -n vmssdemo-vmss --query "[].instanceId" -o tsv
 
 foreach ($id in $ids) {
-
-    Write-Host "Starting stress on VM $id"
-
     az vmss run-command invoke `
-      -g vmss-demo-rg `
-      -n vmssdemo-vmss `
-      --instance-id $id `
-      --command-id RunShellScript `
-      --scripts $script
-
+        -g vmss-demo-rg `
+        -n vmssdemo-vmss `
+        --instance-id $id `
+        --command-id RunShellScript `
+        --scripts "apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y stress-ng && stress-ng --cpu 0 --timeout 600s"
 }
